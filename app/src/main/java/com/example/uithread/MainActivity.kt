bc7a42acb8cf02887e2d9dea095edd41e2c51836
asarangan <60588036+asarangan@users.noreply.github.com>
2020-02-23 21:16:01 -0500

    Copied from https://www.tutorialkart.com/kotlin-android/android-runonuithread-example-kotlin/ Blinkcing lights

--- app/src/main/java/com/example/uithread/MainActivity.kt
+++ app/src/main/java/com/example/uithread/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.uithread
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock.sleep
+import android.widget.Button
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,12 +16,20 @@ class MainActivity : AppCompatActivity() {
         Thread(Runnable {
             // performing some dummy time taking operation
             var i=0;
-            while(i<1000){
+            while(i<100){
                 i++
-                sleep(10)
+
                 this@MainActivity.runOnUiThread(java.lang.Runnable {
-                    this.textview_msg.text = i.toString()
-                })
+                    val id = resources.getIdentifier("textview_msg", "id", this.packageName)
+                    val btn = findViewById<TextView>(id)
+                    btn.setBackgroundResource(R.drawable.red)})
+                    sleep(250)
+                    this@MainActivity.runOnUiThread(java.lang.Runnable {
+                        val id = resources.getIdentifier("textview_msg", "id", this.packageName)
+                        val btn = findViewById<TextView>(id)
+                        btn.setBackgroundResource(R.drawable.green)})
+                        sleep(250)
+
             }
 
             // try to touch View of UI thread
